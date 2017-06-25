a935649f2a718b1dbb4d5a628f6538cdd63747b2
John Aldrin Plata <king.aldrin31@gmail.com>
2017-06-25 21:47:03 +0800

    Update FravelServiceProvider.php

--- src/FravelServiceProvider.php
+++ src/FravelServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Plata\Fravel;
 
+use League\Fractal\Manager;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\View\Factory as ViewFactoryContract;
 
 class FravelServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class FravelServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->bootConfigurationFiles();
+
+        $this->commands('command.make.transformer');
     }
 
     /**
@@ -23,6 +27,80 @@ class FravelServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->registerFacade();
+
+        $this->registerFractalManager();
+
+        $this->registerFravelResponse();
+
+        $this->registerTransformerGenerator();
+    }
+
+    private function registerFacade()
+    {
+        $this->app->singleton('Fractal', function () {
+            return new FractalResourceFactory();
+        });
+    }
+
+    private function registerFractalManager()
+    {
+        $this->app->singleton(Manager::class, function () {
+            $manager = new Manager();
+            $url = \URL::to(config('fravel.base_link', '/'));
+            $serializer = config('fravel.serializer', \League\Fractal\Serializer\DataArraySerializer::class);
+
+            $manager->setSerializer(new $serializer($url));
+
+            return $manager;
+        });
+    }
+
+    private function registerFravelResponse()
+    {
+        \Response::swap(new \Plata\Fravel\Response(
+            $this->app[ViewFactoryContract::class],
+            $this->app['redirect'],
+            $this->app[Manager::class]
+        ));
+    }
+
+    private function registerTransformerGenerator()
+    {
+        $this->app->singleton('transformer.creator', function ($app) {
+            return new TransformerCreator($app['files']);
+        });
+
+        $this->app->singleton('command.make.transformer', function ($app) {
+            // Once we have the migration creator registered, we will create the command
+            // and inject the creator. The creator is responsible for the actual file
+            // creation of the transformers, and may be extended by developers.
+            $creator = $app['transformer.creator'];
+
+            $composer = $app['composer'];
+
+            return new TransformerMakeCommand($creator, $composer);
+        });
+    }
+
+    private function bootConfigurationFiles()
+    {
+        $this->publishes([
+            __DIR__.'/config/config.php' => app()->basePath() . '/config/fravel.php',
+        ]);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'Fractal',
+            'transformer.creator',
+            'command.make.transformer'
+        ];
     }
 }
